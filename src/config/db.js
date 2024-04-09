54a13b6d017fbfa054ddd1bf67469650fc8add07
Salvador Murillo <138176701+SalvadorMurillo@users.noreply.github.com>
2024-04-09 10:29:03 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = "mongodb://localhost:27017/login";
+const MONGO_URL = "mongodb+srv://salvadormurillo612:yp8PkE3AyHmohEEL@cluster0.uy9ifkk.mongodb.net/login4?retryWrites=true&w=majority&appName=Cluster0";
 
 export const connectDB = async () => {
   try {
